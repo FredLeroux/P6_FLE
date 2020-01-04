c5b58409e6ecbb885645920467ae2f6652cf5908
Frederic Leroux <f.leroux2903@gmail.com>
2020-01-04 13:03:55 +0100

    Init commit on branch HomePageDesign

--- 01_SourceCode/01_Java/LesAmisDelEscalade/src/std/fle/_05_controller/_01_HomeController/HomeController.java
+++ 01_SourceCode/01_Java/LesAmisDelEscalade/src/std/fle/_05_controller/_01_HomeController/HomeController.java
@@ -2,6 +2,7 @@ package std.fle._05_controller._01_HomeController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -10,8 +11,8 @@ public class HomeController {
 	
 	
 	@RequestMapping("/home")
-	public String homeDisplay() {
-		return "/01_home/index";
+	public ModelAndView homeDisplay() {
+		return new ModelAndView ("01_home/index");
 		
 	}
 }
